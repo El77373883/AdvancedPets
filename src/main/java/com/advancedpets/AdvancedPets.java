45566ed10b2f2f74325ad2b4a9d57fd74361719d
SoyAdrianYT001 <100530918+El77373883@users.noreply.github.com>
2026-03-18 18:12:00 -0600

    Create AdvancedPets.java

---
+++ src/main/java/com/advancedpets/AdvancedPets.java
@@ -0,0 +1,104 @@
+package com.advancedpets;
+
+import com.advancedpets.commands.PetCommand;
+import com.advancedpets.listeners.CombatListener;
+import com.advancedpets.listeners.PetListener;
+import com.advancedpets.listeners.PlayerListener;
+import com.advancedpets.managers.*;
+import com.advancedpets.utils.MessageUtils;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AdvancedPets extends JavaPlugin {
+
+    private static AdvancedPets instance;
+    private Economy economy;
+    private PetManager petManager;
+    private EconomyManager economyManager;
+    private HologramManager hologramManager;
+    private WorkManager workManager;
+    private ClanManager clanManager;
+    private MissionManager missionManager;
+    private AchievementManager achievementManager;
+    private PlaceholderManager placeholderManager;
+    private MessageUtils messageUtils;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        messageUtils = new MessageUtils(this);
+
+        printStartupMessage();
+
+        if (!setupEconomy()) {
+            getLogger().severe("Vault no encontrado! Desactivando AdvancedPets...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        petManager = new PetManager(this);
+        economyManager = new EconomyManager(this, economy);
+        hologramManager = new HologramManager(this);
+        workManager = new WorkManager(this);
+        clanManager = new ClanManager(this);
+        missionManager = new MissionManager(this);
+        achievementManager = new AchievementManager(this);
+
+        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            placeholderManager = new PlaceholderManager(this);
+            placeholderManager.register();
+            getLogger().info("PlaceholderAPI conectado correctamente!");
+        }
+
+        getServer().getPluginManager().registerEvents(new PetListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        getServer().getPluginManager().registerEvents(new CombatListener(this), this);
+
+        getCommand("ap").setExecutor(new PetCommand(this));
+        getCommand("ap").setTabCompleter(new PetCommand(this));
+
+        getLogger().info("AdvancedPets activado correctamente!");
+    }
+
+    @Override
+    public void onDisable() {
+        if (petManager != null) petManager.saveAllPets();
+        if (hologramManager != null) hologramManager.removeAllHolograms();
+        getLogger().info("AdvancedPets desactivado. Hasta luego!");
+    }
+
+    private void printStartupMessage() {
+        Bukkit.getConsoleSender().sendMessage("§r");
+        Bukkit.getConsoleSender().sendMessage("§6§l✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦");
+        Bukkit.getConsoleSender().sendMessage("§e§l       ⭐ ADVANCED PETS PREMIUM ⭐");
+        Bukkit.getConsoleSender().sendMessage("§6§l✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦");
+        Bukkit.getConsoleSender().sendMessage("§f  Plugin: §eAdvancedPets §6v1.0.0");
+        Bukkit.getConsoleSender().sendMessage("§f  Autor:  §6§lsoyadrianyt001");
+        Bukkit.getConsoleSender().sendMessage("§f  Estado: §a§lACTIVADO ✔");
+        Bukkit.getConsoleSender().sendMessage("§6§l✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦✦");
+        Bukkit.getConsoleSender().sendMessage("§r");
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager()
+                .getRegistration(Economy.class);
+        if (rsp == null) return false;
+        economy = rsp.getProvider();
+        return economy != null;
+    }
+
+    public static AdvancedPets getInstance() { return instance; }
+    public Economy getEconomy() { return economy; }
+    public PetManager getPetManager() { return petManager; }
+    public EconomyManager getEconomyManager() { return economyManager; }
+    public HologramManager getHologramManager() { return hologramManager; }
+    public WorkManager getWorkManager() { return workManager; }
+    public ClanManager getClanManager() { return clanManager; }
+    public MissionManager getMissionManager() { return missionManager; }
+    public AchievementManager getAchievementManager() { return achievementManager; }
+    public MessageUtils getMessageUtils() { return messageUtils; }
+}
